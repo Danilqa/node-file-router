cb6cb8d1ea1c4ec7d4325addc3c219de78a30bef
Daniil Sitdikov <hello.boriskas@gmail.com>
2023-05-08 21:11:23 +0400

    test(slug): change name of suites, simplified tests

--- tests/slug-param.test.js
+++ tests/slug-param.test.js
@@ -2,14 +2,14 @@ import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { decodeSlugParam, encodeSlugParam } from '../src/lib/slug-param/slug-param.js';
 
-const slugParam = suite('SlugParam');
+const SlugParam = suite('SlugParam');
 
-slugParam('should encode a slug param', async (context) => {
+SlugParam('should encode a slug param', async (context) => {
   assert.match(encodeSlugParam('slugId'), new RegExp('^slugId_\\d{8}$'));
 });
 
-slugParam('should decode a slug param', async (context) => {
+SlugParam('should decode a slug param', async (context) => {
   assert.match(decodeSlugParam('slugId_12345678'), 'slugId');
 });
 
-slugParam.run();
+SlugParam.run();
