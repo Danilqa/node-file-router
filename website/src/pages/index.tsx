92145729f1810843a0c842822e36ce696aec199b
Daniil Sitdikov <hello.boriskas@gmail.com>
2023-08-08 09:39:19 +0200

    feat(website): replace meta description

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
+      description="A file-based routing for Node.js">
       <HomepageHeader/>
       <main>
         <HomepageFeatures/>
