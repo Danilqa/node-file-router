03f2d094ca20a4c52ad4a3ed1681ee59e219cc9c
Daniil Sitdikov <hello.boriskas@gmail.com>
2023-11-11 07:08:37 +0100

    refactor(tests): improve code formatting and structure

--- tests/clear-import-cache.test.ts
+++ tests/clear-import-cache.test.ts
@@ -1,72 +1,77 @@
 import { initFileRouter } from '../src/file-router';
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import {
+  describe,
+  it,
+  expect,
+  vi,
+  beforeEach,
+  afterEach,
+  beforeAll
+} from 'vitest';
 
-const mocks = vi.hoisted(() => ({
-  isCommonJs: vi.fn()
-}));
+const mocks = vi.hoisted(() => ({ isCommonJs: vi.fn() }));
 
-vi.mock('/src/utils/env.utils', () => ({
-  isCommonJs: mocks.isCommonJs
-}));
+vi.mock('/src/utils/env.utils', () => ({ isCommonJs: mocks.isCommonJs }));
 
 describe('ClearImportCache', () => {
-  describe('#CommonJS', () => {
-    const testModulePath = `${__dirname}/api-clear-import-cache/fluid-route.ts`;
-    const testModule = { id: '123' } as NodeModule;
+  const baseDir = `${__dirname}/api-clear-import-cache`;
+  const testModulePath = `${__dirname}/api-clear-import-cache/fluid-route.ts`;
+  const testModule = { id: '123' } as NodeModule;
 
+  beforeEach(() => {
+    vi.resetModules();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  describe('#CommonJS', () => {
     beforeEach(() => {
-      vi.resetModules();
+      mocks.isCommonJs.mockReturnValue(true);
       require.cache[testModulePath] = testModule;
     });
 
-    afterEach(() => {
-      vi.restoreAllMocks();
-    });
-
     it('should not remove module from cache by default', async () => {
-      mocks.isCommonJs.mockReturnValue(false);
-
-      await initFileRouter({
-        baseDir: `${__dirname}/api-clear-import-cache`
-      });
+      await initFileRouter({ baseDir });
 
       expect(require.cache[testModulePath]).toBe(testModule);
     });
 
     it('should remove cached module from cache when option is set', async () => {
-      mocks.isCommonJs.mockReturnValue(true);
-
-      await initFileRouter({
-        baseDir: `${__dirname}/api-clear-import-cache`,
-        clearImportCache: true
-      });
+      await initFileRouter({ baseDir, clearImportCache: true });
 
       expect(require.cache[testModulePath]).toBeUndefined();
     });
   });
 
   describe('#ESModules', () => {
-    beforeEach(() => {
-      vi.resetModules();
+    beforeAll(() => {
+      mocks.isCommonJs.mockReturnValue(false);
     });
 
     afterEach(() => {
       vi.restoreAllMocks();
     });
 
-    it('should warn when current environment is not supported', async () => {
-      mocks.isCommonJs.mockReturnValue(false);
+    it('should not warn by default', async () => {
       const warnSpy = vi.spyOn(console, 'warn');
 
-      await initFileRouter({
-        baseDir: `${__dirname}/api-clear-import-cache`,
-        clearImportCache: true
-      });
+      await initFileRouter({ baseDir });
+
+      expect(warnSpy).not.toHaveBeenCalled();
+      warnSpy.mockRestore();
+    });
+
+    it('should warn when current environment is not supported', async () => {
+      const warnSpy = vi.spyOn(console, 'warn');
+
+      await initFileRouter({ baseDir, clearImportCache: true });
 
       expect(warnSpy).toHaveBeenCalledWith(
         'Cache clearing is only supported for CommonJS modules'
       );
-      warnSpy.mockRestore();
+      expect(warnSpy).toHaveBeenCalledTimes(1);
     });
   });
 });
