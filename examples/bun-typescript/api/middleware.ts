10078cb27d710f15b161ec0bfc9fc997ffc34afc
Daniil Sitdikov <hello.boriskas@gmail.com>
2024-02-23 21:05:24 +0100

    feat(examples/bun-typescript): remove extra cors middleware

--- examples/bun-typescript/api/middleware.ts
+++ examples/bun-typescript/api/middleware.ts
@@ -1,9 +1,7 @@
 import { useLogger } from '../middlewares/logger.ts';
 import { useErrorHandler } from '../middlewares/error-handler.ts';
-import { useCors } from '../middlewares/cors.ts';
 
 export default [
   useErrorHandler,
-  useCors,
   useLogger,
 ];
